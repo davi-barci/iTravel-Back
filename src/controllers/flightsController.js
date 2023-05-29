b11a4cc21be77c6890ec4b0ef89af09441966eff
Davi Barci <davibarci0208@gmail.com>
2023-05-29 15:17:07 -0300

    feat: add get flight by id function

--- src/controllers/flightsController.js
+++ src/controllers/flightsController.js
@@ -15,6 +15,8 @@ export async function getFlights(req, res) {
             f."arrivalForecast",
             f.price,
             ap_origin.name AS originAirport,
+            ap_origin.acronym AS originAirportAcronym,
+            ap_destination.acronym AS destinationAirportAcronym,
             ap_destination.name AS destinationAirport,
             a.logo AS airlineLogo,
             f.image
@@ -31,11 +33,52 @@ export async function getFlights(req, res) {
         JOIN
             airlines AS a ON f.airline = a.id
         WHERE
-            f."originCityId" = $1
+            (f."originCityId" = $1 OR $1 = 0)
             AND f."destinationCityId" = $2
-            AND f.price BETWEEN $3 AND $4;`, [originCity, destinationCity, minValue, maxValue]);
-        if (!flights.rowCount) return res.sendStatus(404);
+            AND f.price BETWEEN $3 AND $4;
+    `, [originCity, destinationCity, minValue, maxValue]);
         return res.status(200).send(flights.rows);
+    } catch (err) {
+        return res.status(500).send(err.message);
+    }
+};
+
+
+export async function getFlightById(req, res) {
+    const { id } = req.params;
+
+    try {
+        const flight = await db.query(`
+        SELECT
+            f.id,
+            c_origin.name AS originCity,
+            c_destination.name AS destinationCity,
+            a.name AS airline,
+            f."outputForecast",
+            f."arrivalForecast",
+            f.price,
+            ap_origin.name AS originAirport,
+            ap_origin.acronym AS originAirportAcronym,
+            ap_destination.acronym AS destinationAirportAcronym,
+            ap_destination.name AS destinationAirport,
+            a.logo AS airlineLogo,
+            f.image
+        FROM
+            flights AS f
+        JOIN
+            cities AS c_origin ON f."originCityId" = c_origin.id
+        JOIN
+            cities AS c_destination ON f."destinationCityId" = c_destination.id
+        JOIN
+            airports AS ap_origin ON f."originAirport" = ap_origin.id
+        JOIN
+            airports AS ap_destination ON f."destinationAirport" = ap_destination.id
+        JOIN
+            airlines AS a ON f.airline = a.id
+        WHERE
+            f.id = $1;
+    `, [id]);
+        return res.status(200).send(flight.rows);
     } catch (err) {
         return res.status(500).send(err.message);
     }
--- src/routes/flightsRouter.js
+++ src/routes/flightsRouter.js
@@ -1,8 +1,9 @@
 import {Router} from 'express';
-import { getFlights } from '../controllers/flightsController.js';
+import { getFlightById, getFlights } from '../controllers/flightsController.js';
 
 const flightsRouter = Router();
 
 flightsRouter.get("/flights/:originCity/:destinationCity", getFlights);
+flightsRouter.get("/flights/:id", getFlightById);
 
 export default flightsRouter;
